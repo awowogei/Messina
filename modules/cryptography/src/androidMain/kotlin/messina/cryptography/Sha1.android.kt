068312190a08a523caf54206277233870bf238d6
awowogei <125923016+awowogei@users.noreply.github.com>
2026-06-30 21:32:17 +0200

    Thread safety

--- modules/cryptography/src/androidMain/kotlin/messina/cryptography/Sha1.android.kt
+++ modules/cryptography/src/androidMain/kotlin/messina/cryptography/Sha1.android.kt
@@ -2,9 +2,6 @@ package messina.cryptography
 
 import java.security.MessageDigest
 
-
-private val sha1Digest = MessageDigest.getInstance("SHA-1")
-
 actual fun sha1(data: ByteArray): ByteArray {
-    return sha1Digest.digest(data)
+    return MessageDigest.getInstance("SHA-1").digest(data)
 }
--- modules/cryptography/src/androidMain/kotlin/messina/cryptography/Sha256.android.kt
+++ modules/cryptography/src/androidMain/kotlin/messina/cryptography/Sha256.android.kt
@@ -2,9 +2,6 @@ package messina.cryptography
 
 import java.security.MessageDigest
 
-
-private val sha256Digest = MessageDigest.getInstance("SHA-256")
-
 actual fun sha256(data: ByteArray): ByteArray {
-    return sha256Digest.digest(data)
+    return MessageDigest.getInstance("SHA-256").digest(data)
 }
